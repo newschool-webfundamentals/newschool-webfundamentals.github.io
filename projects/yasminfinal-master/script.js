ac9dec06eb9105d202b8cb468030c805691ee87e
Alexandra Ackerman <aaackerman.dev@gmail.com>
2017-08-27 12:57:18 -0400

    Update introduction and add recent final

---
+++ projects/yasminfinal-master/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+ $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+  $(document).ready(function(){
+      $('.slider').slider();
+    });
+});
